e032116a5e0dd51c64745e88eed4829f8ee356a0
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2021-02-07 16:55:43 -0600

    major update to palette (now a list with 3 items, for each palette) and improved show.gpPal()

--- ggGalactic.R
+++ ggGalactic.R
@@ -1,73 +1,63 @@
 #Some code modified from https://bbc.github.io/rcookbook/#how_to_create_bbc_style_graphics
 if(!require(pacman))install.packages("pacman")
-pacman::p_load(ggplot2,sysfonts,viridis,scales,showtext,png,RCurl,grid,tidyverse)
+pacman::p_load(ggplot2,sysfonts,viridis,scales,showtext,png,RCurl,grid,tidyverse,colorspace)
 showtext_auto()
 #GP Palette
-gpPal=c(
-  "#2c83c3", #1 Hydrogen Blue
-  "#6c2d82", #2 Lightning Purple
-  "#ff3dac", #3 Flare Fucsia
-  "#6812d1", #4 Atomic Blue
-  "#cb1f8e", #5 Burst Purple
-  "#090816", #6 Galactic Black
-  "#f0f4ff" #7 Sparkle White
-)
-#expanded palette
-gpPal2=c(
-  "#2c83c3", #1 Hydrogen Blue
-  "#69b2f6",   #1a light blue
-  "#005792",   #1b dark blue
-  "#C36C2C",  #1c complementary orange
-  "#6c2d82", #2 Lightning Purple
-  "#9c5ab2",    #2a light Purple
-  "#3e0055",   #2b dark purple
-  "#43822D",   #2c complementary dark green
-  "#ff3dac", #3 Flare Fucsia
-  "#ff78de",   #3a light fucsia
-  "#c7007d",   #3b dark fuscia
-  "#3DFF90",   #3c complementary bright green
-  "#6812d1", #4 Atomic Blue
-  "#9f4bff",   #4a light atomic blue
-  "#29009e",   #4b dark atomic blue
-  "#7BD112",   #4c complementary neon green
-  "#cb1f8e", #5 Burst Purple
-  "#ff5dbe",   #5a light burst purple
-  "#960060",   #5b dark burst purple
-  "#cb1f8e",   #5c complementary green
-  "#090816", #6 Galactic Black
-  "#f0f4ff" #7 Sparkle White
-  
+gpPal=list(main=
+  data.frame(name=c("hydrogen blue","lightning purple","flare fucsia","atomic blue","burst purple", "galactic black","sparkle white"),hex=c("#2c83c3","#6c2d82","#ff3dac","#6812d1","#cb1f8e","#363636","#f0f4ff"),rgb=c("rgb(44,131,195,maxColorValue=255)","rgb(108,45,130,maxColorValue=255)","rgb(255,61,172,maxColorValue=255)","rgb(104,18,209,maxColorValue=255)","rgb(203,31,142,maxColorValue=255)","rgb(54,54,54,maxColorValue=255)","rgb(240,244,255,maxColorValue=255)")),
+  extended=data.frame(name=c("dark hydrogen blue","hydrogen blue","light hydrogen blue","dark lightning purple","lightning purple","light lightning purple","flare fucsia","light flare fucsia","pale flare fucsia","dark atomic blue","atomic blue","light atomic blue","dark burst purple","burst purple","light burst purple","galactic black","sparkle white"),
+                      hex=c("#005792","#2c83c3","#69b2f6","#3E0D55","#6c2d82","#9c5ab2","#ff3dac","#ff5dbe","#ff78de","#291c9e","#6812d1","#9f4bff","#96195F","#cb1f8e","#c7247d","#363636","#f0f4ff"),
+                      rgb=c("rgb(0,87,46,maxColorValue=255)","rgb(44,131,195,maxColorValue=255)","rgb(105,178,246,maxColorValue=255)","rgb(62,513,85,maxColorValue=255)","rgb(108,45,130,maxColorValue=255)","rgb(156,90,178,maxColorValue=255)","rgb(255,61,172,maxColorValue=255)","rgb(255,93,190,maxColorValue=255)","rgb(255,120,222,maxColorValue=255)","rgb(41,28,158,maxColorValue=255)","rgb(104,18,209,maxColorValue=255)","rgb(159,75,255,maxColorValue=255)","rgb(150,25,95,maxColorValue=255)","rgb(203,31,142,maxColorValue=255)","rgb(199,36,125,maxColorValue=255)","rgb(54,54,54,maxColorValue=255)","rgb(240,244,255,maxColorValue=255)")),
+  subjects=data.frame(
+    names=c("math","ela","socstudies","science","extra"),
+    hex=c("#db4125","#eca14d","#633a9a","#b798e8","#f4f0d9"),
+    rgb=c("rgb(219,65,37,maxColorValue=255))","rgb(236,161,77,maxColorValue=255))","rgb(99,58,154,maxColorValue=255))","rgb(183,152,232,maxColorValue=255)","rgb(244,240,217,maxColorValue=255))"))
   )
-
+  
+      
 #Show the GP palette
-# x is the first n colors to show or x1:x2 or c(1,3,5) for a specific subset
+# subset is the first n colors to show or x1:x2 or c(1,3,5) for a specific subset
 # pal is 1 by default; if 2, shows extended palette
-show.gpPal<-function(pal=1,x){
-  P <- if(pal==1){gpPal}else{gpPal2}
-  if(missing(x)){x=length(P)}
-  s <- if(length(x)==1){eval(quote(1:x))}else{eval(quote(x))}
-  scales::show_col(P[s])}
+show.gpPal<-function(pal=1,subset=NULL,...){
+  P<-gpPal[[pal]]$hex
+  if(length(subset!=0)){
+    s <- if(length(subset)==1){eval(quote(1:subset))}else{eval(quote(subset))}
+    labs<-subset
+    }else{
+      s=1:length(P)
+      labs<-1:length(P)}
+  nColors<-length(s)
+  Pnames<-gpPal[[pal]]$name
+  Ptitle<-names(gpPal)[pal]
+  
+  
+  swatchplot(P[s],...)
+  whereAtStart<-1/nColors/2
+  whereAt<-seq(whereAtStart,1-whereAtStart,1/nColors)
+  mtext(labs,side=1,line=0,at=whereAt)
+  mtext(paste0("palette = ",Ptitle),side=3,line=2,0,font=1)
+  }
+
 
 #These are default shapes
 gpShps<-c(21,24,22,23,8,13,9)
 plot(0:length(gpShps),0:length(gpShps),pch=gpShps,cex=2,xlim=c(-1,length(gpShps)+1),ylim=c(-1,length(gpShps)+1))
   
-font="Montserrat"
-  fam=font
-  font_add_google(name=font,family=fam)
 
-ggGalactic<-function(){
-  font="Montserrat"
+ggGalactic<-function(font="Montserrat",regular.wt=400,bold.wt=700,font.cex=1,plot.margin=margin(t=10,r=10,b=10,l=10)){
+  fam=font
+  font_add_google(name=font,family=fam,regular.wt=regular.wt,bold.wt=bold.wt)
 theme_linedraw()+theme(
     text=element_text(family=font),
-    plot.title=element_text(family=font,size=30,face="bold",color=gpPal[6]),
-    plot.subtitle=element_text(family=font,size=22,color=gpPal[5]),
-    axis.title=element_text(family=font,size=28,face="bold",color=gpPal[6]),
-    axis.text=element_text(family=font,size=18,color=gpPal[6]),
+    plot.margin=plot.margin,
+    plot.title=element_text(family=font,size=30*font.cex,face="bold",color=gpPal[6]),
+    plot.subtitle=element_text(family=font,size=22*font.cex,color=gpPal[5]),
+    axis.title=element_text(family=font,size=28*font.cex,face="bold",color=gpPal[6]),
+    axis.text=element_text(family=font,size=18*font.cex,color=gpPal[6]),
     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
     axis.title.y = element_text(margin = margin(t = 20, r = 10, b = 0, l = 0)),
-    legend.text=element_text(family=font,color=gpPal[6],size=18),
-    legend.title=element_text(family=font,color=gpPal[6],face="bold",size=18),
+    legend.text=element_text(family=font,color=gpPal[6],size=18*font.cex),
+    legend.title=element_text(family=font,color=gpPal[6],face="bold",size=18*font.cex),
     legend.position = "right", legend.text.align = 0, legend.background =element_blank()
   )
 }
