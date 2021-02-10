8822a558050506df6d8bfb9f1e365e362e9a3259
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2021-02-10 11:49:57 -0600

    added gpColors function for extracting a vector of hex values for named gp colors

--- ggGalactic.R
+++ ggGalactic.R
@@ -9,7 +9,7 @@ gpPal=list(main=
                       hex=c("#005792","#2c83c3","#69b2f6","#3E0D55","#6c2d82","#9c5ab2","#ff3dac","#ff5dbe","#ff78de","#291c9e","#6812d1","#9f4bff","#96195F","#cb1f8e","#c7247d","#363636","#f0f4ff"),
                       rgb=c("rgb(0,87,46,maxColorValue=255)","rgb(44,131,195,maxColorValue=255)","rgb(105,178,246,maxColorValue=255)","rgb(62,513,85,maxColorValue=255)","rgb(108,45,130,maxColorValue=255)","rgb(156,90,178,maxColorValue=255)","rgb(255,61,172,maxColorValue=255)","rgb(255,93,190,maxColorValue=255)","rgb(255,120,222,maxColorValue=255)","rgb(41,28,158,maxColorValue=255)","rgb(104,18,209,maxColorValue=255)","rgb(159,75,255,maxColorValue=255)","rgb(150,25,95,maxColorValue=255)","rgb(203,31,142,maxColorValue=255)","rgb(199,36,125,maxColorValue=255)","rgb(54,54,54,maxColorValue=255)","rgb(240,244,255,maxColorValue=255)")),
   subjects=data.frame(
-    names=c("math","ela","socstudies","science","extra"),
+    name=c("math","ela","socstudies","science","extra"),
     hex=c("#db4125","#eca14d","#633a9a","#b798e8","#f4f0d9"),
     rgb=c("rgb(219,65,37,maxColorValue=255))","rgb(236,161,77,maxColorValue=255))","rgb(99,58,154,maxColorValue=255))","rgb(183,152,232,maxColorValue=255)","rgb(244,240,217,maxColorValue=255))"))
   )
@@ -37,6 +37,16 @@ show.gpPal<-function(pal=1,subset=NULL,...){
   mtext(labs,side=1,line=0,at=whereAt)
   mtext(paste0("palette = ",Ptitle),side=3,line=2,0,font=1)
   }
+
+#output a vector of hex codes, given named colors; list=TRUE lists all options
+gpColors<-function(colorNames,list=FALSE){
+  allPal<-subset(do.call(bind_rows,gpPal),!duplicated(name))
+  if(list==TRUE){cat("GP PALETTE OPTIONS",paste0(rep("-",20),collapse=""),"\n",paste(allPal[,1],collapse=", "))}
+  indx=charmatch(colorNames,allPal$name)
+  colVec<-allPal$hex[indx]
+  names(colVec)<-allPal$name[indx]
+  return(colVec)
+}
 
 
 #These are default shapes
